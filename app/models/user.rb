3fc8793723901f209f03f89aa71713dd0486ae49
Scott Whitman <scott.w.whitman@gmail.com>
2016-10-14 13:56:22 -0700

    built users and sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  def self.confirm(params)
+  @user = User.find_by({email: params[:email]})
+  @user ? @user.authenticate(params[:password]) : false
+  end
+  validates :email, uniqueness:true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "users#index"
+
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+
+  get "/users", to: "users#index"
+  get "/users/new", to: "users#new", as: "new_user"
+  get "/users/:id", to:"users#show", as: "user"
+  post "/users", to: "users#create"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  patch "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy", as: "destroy_user"
+
+
+
+
+
 end
