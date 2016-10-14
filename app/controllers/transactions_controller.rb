42e6c18cecfeb0ac070665f5e4e8f0f1a74beabf
Zachary Fong <fong.zachary@gmail.com>
2016-10-14 14:28:08 -0700

    finish associations and building view

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,15 @@
 class TransactionsController < ApplicationController
-  # def show
-  #   @transaction = Transaction
-  # end
+  def show
+    @transaction = Transaction
+  end
 
-  # def create
-  #   @transaction = Transaction.new(transaction_params)
-  # end
+  def create
+    @transaction = Transaction.new(transaction_params)
+    redirect_to root_path
+  end
 
-  # private
-  # def transaction_params
-  #   params.require(:transaction).permit(:stripe_id, :space_id, :renter_id, :start_date, :end_date)
-  # end
+  private
+  def transaction_params
+    params.require(:transaction).permit(:stripe_id, :space_id, :renter_id, :start_date, :end_date)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,4 @@
 class Transaction < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   belongs_to :space
 end
