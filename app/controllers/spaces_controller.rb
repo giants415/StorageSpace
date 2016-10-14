4c10096f90215df698299a12a339366352bc3d9d
Andrew <andrew.vinocur@gmail.com>
2016-10-14 12:47:00 -0700

    creates basic actions of the controller w/ no code

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,23 @@
 class SpacesController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def spaces_params
+
+  end
 end
