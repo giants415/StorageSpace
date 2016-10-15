72d1ab304cc9fbab48507c784bc01fddc4b05f47
Scott Whitman <scott.w.whitman@gmail.com>
2016-10-14 17:15:41 -0700

    added seed data and Nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user1 = User.create({name: "Zach Fong", email: "zach@gmail.com", password: "password"})
+user2 = User.create({name: "Alicia Brown", email: "alicia@gmail.com", password: "password"})
+user3 = User.create({name: "Andrew Vinocur", email: "andrew@gmail.com", password: "password"})
+user4 = User.create({name: "Test Person", email: "test@gmail.com", password: "password"})
+user5 = User.create({name: "Scott Whitman", email: "scott@gmail.com", password: "password"})
+user6 = User.create({name: "Admin Person", email: "admin@gmail.com", password: "admin"})
+user6.admin = true
+user6.save
+
+# space1 = Space.create({user_id: "", })
+#
+#
+# create_table "spaces", force: :cascade do |t|
+#   t.integer  "user_id"
+#   t.string   "title"
+#   t.integer  "price"
+#   t.string   "address"
+#   t.integer  "size"
+#   t.boolean  "available"
+#   t.string   "photo"
+#   t.string   "status"
+#   t.string   "description"
+#   t.datetime "created_at",  null: false
+#   t.datetime "updated_at",  null: false
+# end
