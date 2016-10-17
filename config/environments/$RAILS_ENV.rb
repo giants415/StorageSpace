9b1a05b9997bbee8f9a8ea949925ab248a04dfc6
Andrew <andrew.vinocur@gmail.com>
2016-10-17 14:54:31 -0700

    adds working email confirmation on user sign up, working to hide email PW

--- config/environments/$RAILS_ENV.rb
+++
@@ -1,9 +0,0 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.google.com',
-  port:                 25 || 587,
-  domain:               'storagespacesample@gmail.com',
-  user_name:            'storagespacesample',
-  password:             'GAandrew31',
-  authentication:       'plain',
-  enable_starttls_auto: true  }
