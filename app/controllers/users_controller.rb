78b3716aef39ffe739e363e4db6aff7181df5fb3
Andrew <andrew.vinocur@gmail.com>
2016-10-19 13:48:08 -0700

    adds error message for sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,18 +11,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    respond_to do |format|
       if @user.save
         login(@user)
         redirect_to @user
         WelcomeMailer.welcome_email(@user).deliver_now
-        format.html { redirect_to(@user, notice: 'User was successfully created.') }
-        format.json { render json: @user, status: :created, location: @user }
       else
         flash[:error]= @user.errors.full_messages
         redirect_to new_user_path
       end
-    end
   end
 
   def show
