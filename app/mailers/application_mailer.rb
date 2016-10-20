19abe1591feba00f5f80dc712b8b2ff07f3b144d
Zachary Fong <fong.zachary@gmail.com>
2016-10-19 19:27:05 -0700

    sets default url for url helpers and adds flash message for email confirm

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'storagespacesample@gmail.com'
   layout 'mailer'
+
+  def default_url_options
+    if Rails.env.production?
+      {:host => "localhost:3000"}
+    else
+      {:host => "localhost:3000"}
+    end
+  end
 end
 
 class WelcomeMailer < ApplicationMailer
