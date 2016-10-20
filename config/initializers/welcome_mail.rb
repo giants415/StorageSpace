ef27c59f58ccd0fd89a6f318483f78221619e160
Andrew <andrew.vinocur@gmail.com>
2016-10-19 23:12:43 -0700

    adds env vars to welcome_mail.rb and production.rb

--- config/initializers/welcome_mail.rb
+++ config/initializers/welcome_mail.rb
@@ -2,9 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address      => "smtp.gmail.com",
   :port         => 587,
   :domain       => "gmail.com",
-  :user_name    => "storagespacesample@gmail.com",
-  :password     => Rails.application.secrets.secret_email_pw,
+  :user_name    => ENV["GMAIL_USERNAME"],
+  :password     => ENV["GMAIL_PASSWORD"],
   :authentication => "plain",
   :enable_starttls_auto => true
 }
- 
